5e44178f24e780585a8d85bce15ccfed6b7231aa
Zhang Xiaobai <364772080@qq.com>
2022-10-12 16:01:23 +0800

    win init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let event_loop = winit::event_loop::EventLoop::new();
+    let window = winit::window::WindowBuilder::new()
+        .with_inner_size(winit::dpi::PhysicalSize::new(800, 600))
+        .with_title("Test")
+        .build(&event_loop)
+        .unwrap();
+    event_loop.run(move |event, _, control| match event {
+        winit::event::Event::WindowEvent {
+            window_id,
+            event: winit::event::WindowEvent::CloseRequested,
+        } if window.id() == window_id => {
+            control.set_exit();
+        }
+        winit::event::Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
