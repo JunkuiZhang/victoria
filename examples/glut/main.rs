c6f3288f5b1bf2faff2b7205134b73bf3ef1ec9e
Junkui Zhang <364772080@qq.com>
2022-11-11 21:16:17 +0800

    example update

--- examples/glut/main.rs
+++ examples/glut/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+fn main() {
+    println!("A: {}", u32::from('A'));
+    println!("我: {}", u32::from('我'));
+    let s1 = "Hello world!";
+    for c in s1.chars() {
+        print!("{}", c);
+    }
+    print!("\n");
+    println!(
+        "Char len: {}",
+        std::mem::size_of_val(&s1.chars().nth(0).unwrap())
+    );
+    let s2 = String::from("我是张小白");
+    for c in s2.chars() {
+        print!("{}", c);
+    }
+    print!("\n");
+    println!(
+        "Char len: {}",
+        std::mem::size_of_val(&s2.chars().nth(0).unwrap())
+    );
+}
